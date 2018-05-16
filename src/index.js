78be29831283d9a458d4943d5d47b6d44f2969ca
choarau - Syscea <choarau@syscea.com>
2018-05-16 11:14:51 +0400

    Migration to Hapi 17

--- src/index.js
+++ src/index.js
@@ -14,78 +14,97 @@ const Path = require('path');
 const internals = {};
 
 internals.defaults = {
-  views: {
-    engines: {}
-  },
-  inlineImages: true,
-  inlineStyles: true
+    views: {
+        engines: {}
+    },
+    inlineImages: true,
+    inlineStyles: true
 };
 
 internals.schema = Joi.object({
-  transport: Joi.object(),
-  views: Joi.object(),
-  inlineImages: Joi.boolean(),
-  inlineStyles: Joi.boolean()
+    transport: Joi.object(),
+    views: Joi.object(),
+    inlineImages: Joi.boolean(),
+    inlineStyles: Joi.boolean()
 });
 
-exports.register = function(server, options, next) {
-  Joi.assert(options, internals.schema);
-  const config = Hoek.applyToDefaultsWithShallow(internals.defaults, options, ['views']);
+const pkg = require("../package");
 
-  const transport = Nodemailer.createTransport(config.transport);
+exports.plugin = {
+    name: pkg.name,
+    version: pkg.version,
+    pkg: pkg,
+    once: true,
+    multiple: false,
+    register: function (server, options/*: OptionsI*/) {
 
-  if (config.inlineImages) {
-    transport.use('compile', NodemailerPluginInlineBase64);
-  }
+        Joi.assert(options, internals.schema);
+        const config = Hoek.applyToDefaultsWithShallow(internals.defaults, options, ['views']);
 
-  server.dependency('vision', (server, done) => {
-    if (Object.keys(config.views.engines).length) {
-      server.views(config.views);
-    }
+        const transport = Nodemailer.createTransport(config.transport);
 
-    server.expose('send', (data, callback) => {
-      Items.parallel(['text', 'html'], (format, cb) => {
-        const path = typeof data[format] === 'object' ? data[format].path : '';
-        const extension = Path.extname(path).substr(1);
-
-        if (config.views.engines.hasOwnProperty(extension)) {
-          server.render(path, data.context, (err, rendered) => {
-            if (err) return cb(err);
-
-            if (format === 'html' && config.inlineStyles) {
-              data[format] = Juice(rendered); // eslint-disable-line new-cap
-            } else {
-              data[format] = rendered;
-            }
-
-            return cb();
-          });
-        } else {
-          if (typeof data[format] !== 'object') {
-            return cb();
-          }
-
-          Fs.readFile(path, 'utf8', (err, rendered) => {
-            if (err) return cb(err);
-
-            data[format] = rendered;
-            return cb();
-          });
+        if (config.inlineImages) {
+            transport.use('compile', NodemailerPluginInlineBase64);
         }
-      }, (err) => {
-        if (err) return callback(err);
 
-        delete data.context;
-        transport.sendMail(data, callback);
-      });
-    });
+        // server.dependency('vision', (server, done) => {
+        if (Object.keys(config.views.engines).length) {
+            server.views(config.views);
+        }
 
-    done();
-  });
+        server.expose('sendMail',
+            async (data) => {
+                let promise = new Promise(
+                    (resolve, reject) => {
+                        Items.parallel(['text', 'html'], (format, cb) => {
+                            const path = typeof data[format] === 'object' ? data[format].path : '';
+                            const extension = Path.extname(path).substr(1);
 
-  next();
-};
+                            if (config.views.engines.hasOwnProperty(extension)) {
+                                server.render(path, data.context, (err, rendered) => {
+                                    if (err) return cb(err);
 
-exports.register.attributes = {
-  name: 'hapi-mailer'
+                                    if (format === 'html' && config.inlineStyles) {
+                                        data[format] = Juice(rendered); // eslint-disable-line new-cap
+                                    } else {
+                                        data[format] = rendered;
+                                    }
+
+                                    return cb();
+                                });
+                            } else {
+                                if (typeof data[format] !== 'object') {
+                                    return cb();
+                                }
+
+                                Fs.readFile(path, 'utf8', (err, rendered) => {
+                                    if (err) return cb(err);
+
+                                    data[format] = rendered;
+                                    return cb();
+                                });
+                            }
+                        }, (err) => {
+                            if (err) return reject(err);
+
+                            delete data.context;
+                            transport.sendMail(
+                                data,
+                                (err, info) => {
+                                    server.log("debug", info);
+                                    if (err) {
+                                        return reject(err)
+                                    }
+
+                                    return resolve(info);
+                                }
+                            );
+                        });
+                    }
+                );
+
+                return promise;
+            }
+        );
+    }
 };
